1847337f813f712cb9839cfc1ff921204e5ae301
Кирилл Вегеле <kirove.work@gmail.com>
2026-05-07 00:24:43 +0300

    deploy 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     react(),
     VitePWA({
