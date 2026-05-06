576e8b2dd92a62f1c16e2ef78f7e3e45bd382004
Кирилл Вегеле <kirove.work@gmail.com>
2026-05-07 00:57:11 +0300

    restore normal PWA after SW cleanup

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      selfDestroying: true,
       registerType: 'autoUpdate',
       manifest: {
         name: 'Госуслуги',
